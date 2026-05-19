3224555d1588c51a0a09816b6f7f0f9b202476ff
Pizzetta1 <albertomargaritella0@gmail.com>
2026-05-19 17:01:01 +0200

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -199,15 +199,12 @@ export default function Home() {
       <Navbar />
 
       {/* HERO */}
-     <section
-  ref={heroRef}
-  className="relative md:relative fixed md:top-auto top-0 left-0 w-full h-[115vh] md:h-auto overflow-hidden"
->
+     <section ref={heroRef} className="relative overflow-hidden group">
 
         <img
           ref={imageRef}
           src="/hero.png"
-          className="w-full h-full object-cover object-top"
+          className="w-full h-[110vh] md:h-[150vh] object-cover object-top"
           alt="hero"
         />
 
@@ -265,7 +262,7 @@ export default function Home() {
 
       </section>
 
-      <div className="relative z-30 bg-[#F5F5F5] mt-[100vh] md:mt-0 rounded-t-3xl">
+     <div className="relative z-20 bg-[#F5F5F5] -mt-[20vh] md:mt-0 rounded-t-3xl pt-6 shadow-[0_-10px_30px_rgba(0,0,0,0.12)]">
 
         <section className="py-32 px-6 border-t border-black/5">
           <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-16 items-end">
